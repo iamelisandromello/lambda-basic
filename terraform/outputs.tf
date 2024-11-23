a320d80fe2eb78414609bac8aa9459477c095e7f
Elisandro Mello Correa <elisandro@brandlovrs.com>
2024-11-23 11:18:57 -0300

    refactor: adjust files terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "lambda_arn" {
   value = aws_lambda_function.my_lambda_function.arn
 }
 
-output "bucket_suffix" {
-  value = random_id.bucket_suffix.hex
+output "bucket_name" {
+  value = aws_s3_bucket.lambda_code_bucket.bucket
 }
