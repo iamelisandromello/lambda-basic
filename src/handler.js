f728ff9a14e06273e19cfde0754862110d14d0a0
Elisandro Mello Correa <elisandro@brandlovrs.com>
2024-11-23 22:59:04 -0300

    refactor: update handler

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,13 @@
-exports.handler = async (event) => {
+module.exports.hello = async (event) => {
   return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, World!" }),
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
   };
 };
